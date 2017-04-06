2daa12d92052aaee86b38a7005e858b3e6bef04b
jackbackrack <jrb@pobox.com>
2017-04-06 09:59:00 -0700

    add/fix capture/abuttment joinery, order linear capture joinery first, split out plates

--- Makefile.mk
+++ Makefile.mk
@@ -19,4 +19,15 @@ ${GEN}/ao-gfx.pkg: stanza-ao/ao-gfx.stanza ${GEN}/gl.pkg ${GEN}/geom.pkg ${GEN}/
 ${GEN}/ao-eval.pkg: stanza-ao/ao-eval.stanza ${GEN}/utils.pkg ${GEN}/eval.pkg ${GEN}/geom.pkg ${GEN}/glu.pkg ${GEN}/ao.pkg ${GEN}/font.pkg ${GEN}/eval-ao.pkg
 	stanza $< $(STZ_FLAGS)
 
+${GEN}/plates.pkg: stanza-ao/plates.stanza ${GEN}/utils.pkg ${GEN}/geom.pkg ${GEN}/ao.pkg
+	stanza $< $(STZ_FLAGS)
+
+${GEN}/eval-plates.stanza: ${GEN}/gen-repl ${GEN}/plates.pkg
+	${GEN}/gen-repl plates
+
+${GEN}/eval-plates.pkg: ${GEN}/eval-plates.stanza ${BASE_EVAL_PKGS} ${GEN}/eval-utils.pkg ${GEN}/eval-geom.pkg ${GEN}/eval-font.pkg ${GEN}/eval-glu.pkg ${GEN}/eval-ao.pkg ${GEN}/plates.pkg
+	stanza $< $(STZ_FLAGS)
+
+${GEN}/plates-eval.pkg: stanza-ao/plates.stanza ${GEN}/utils.pkg ${GEN}/eval.pkg ${GEN}/geom.pkg ${GEN}/glu.pkg ${GEN}/ao.pkg ${GEN}/eval-ao.pkg ${GEN}/plates.pkg ${GEN}/eval-plates.pkg 
+	stanza $< $(STZ_FLAGS)
 
